12a6e70b63a749ea57b4defd72a1db8657a745b0
Matej Šándor <matejko.sandor@gmail.com>
2020-06-11 10:03:52 +0200

    add basic movement

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ TEXT_SCORE = "Score: "
 
 
 class Snake:
-    def __init__(self, x, y, vel):
+    def __init__(self, x, y, vel_hor, vel_ver):
         self.x = x
         self.y = y
-        self.vel = 0
+        self.vel_hor = vel_hor
+        self.vel_ver = vel_ver
 
     def draw_snake(self, x, y, win):
         self.x = x
         self.y = y
-        pygame.draw.rect(win, (36, 252, 3), (x, y, 15, 15))
+        lead_rect = pygame.Rect((x, y, 15, 15))
+        pygame.draw.rect(win, (36, 252, 3), lead_rect)
 
 
 def draw_window(snake, win):
@@ -31,13 +33,34 @@ def draw_window(snake, win):
 
 def main():
     loop = True
+    snake = Snake(300, 300, 0, 0)
+    clock = pygame.time.Clock()
+
     while loop:
         window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-        draw_window(Snake(100, 100, 0), window)
+        draw_window(snake, window)
+        clock.tick(5)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 loop = False
                 quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    snake.vel_ver = 0
+                    snake.vel_hor = -15
+                if event.key == pygame.K_RIGHT:
+                    snake.vel_ver = 0
+                    snake.vel_hor = 15
+                if event.key == pygame.K_DOWN:
+                    snake.vel_hor = 0
+                    snake.vel_ver = 15
+                if event.key == pygame.K_UP:
+                    snake.vel_hor = 0
+                    snake.vel_ver = -15
+
+        snake.x += snake.vel_hor
+        snake.y += snake.vel_ver
 
 
 if __name__ == '__main__':
